66e7b4260d363547afc2417f6cc92a931dd1c6f5
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-04-01 01:19:50 +0500

    Create Get Post Api With Pagination Functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { verifyUser } from '../errors/verifyUser.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPosts } from '../controllers/post.controller.js';
 
 const router= express.Router();
 
 router.post('/create',verifyUser,createPost);
-
+router.get('/getposts',verifyUser,getPosts);
 export default router;
